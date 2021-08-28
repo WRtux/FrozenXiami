9ae25dbf0aa418e78e4548b1716654c82953ee8a
WRtux <wildernessrgr@aliyun.com>
2021-08-28 22:27:52 +0800

    Trivial adjustment
    Optimize cover image and adjust some parts (non-functional).

--- source/web/page.js
+++ source/web/page.js
@@ -1,18 +1,30 @@
 "use strict";
 
+var page = {
+	main: document.getElementById("main"),
+	masks: {
+		toast: document.getElementById("mask-toast"),
+		dialog: document.getElementById("mask-dialog")
+	},
+	templates: {
+		main: document.getElementById("main-template").content,
+		toast: document.getElementById("toast-template").content,
+		dialog: document.getElementById("dialog-template").content
+	}
+};
+
 function toast(inf, t) {
-	let tmpl = document.getElementById("toast-template").content;
-	let ele = document.importNode(tmpl.firstElementChild, true);
+	let ele = document.importNode(page.templates.toast.firstElementChild, true);
 	ele.addEventListener("click", (e) => { clearToast(ele); });
 	ele.firstElementChild.textContent = inf;
-	document.getElementById("mask-toast").appendChild(ele);
+	page.masks.toast.appendChild(ele);
 	window.setTimeout(() => { clearToast(ele); }, t || 2000);
 	return ele;
 }
 
 function clearToast(ele) {
 	ele.classList.add("toast-clear");
-	window.setTimeout(() => { ele.parentElement && ele.parentElement.removeChild(ele); }, 300);
+	window.setTimeout(() => { ele.parentElement && ele.remove(); }, 300);
 }
 
 function openDialog(n) {
@@ -20,15 +32,14 @@ function openDialog(n) {
 	if (dlg != null) {
 		ele = dlg.parentElement;
 	} else {
-		let tmpl = document.getElementById("dialog-template").content;
-		ele = document.importNode(tmpl.firstElementChild, true);
+		ele = document.importNode(page.templates.dialog.firstElementChild, true);
 		dlg = ele.firstElementChild;
 		ele.classList.add("block-dynamic");
 		dlg.id = "dialog-" + n;
 	}
 	ele.classList.add("block-active");
 	dlg.querySelector(".dialog> .dialog-close").addEventListener("click", (e) => { closeDialog(dlg); });
-	document.getElementById("mask-dialog").appendChild(ele);
+	page.masks.dialog.appendChild(ele);
 	return dlg;
 }
 function alertDialog(tt, inf, blk) {
@@ -36,10 +47,7 @@ function alertDialog(tt, inf, blk) {
 	let dlg = openDialog(n);
 	dlg.querySelector(".dialog> h2").textContent = tt || "提示";
 	dlg.querySelector(".dialog> p").textContent = inf;
-	if (blk) {
-		let ele = dlg.querySelector(".dialog> .dialog-close");
-		ele.parentElement.removeChild(ele);
-	}
+	blk && dlg.querySelector(".dialog> .dialog-close").remove();
 	return dlg;
 }
 
@@ -53,16 +61,16 @@ function closeDialog(dlg) {
 	dlg = dlg instanceof Element ? dlg : document.getElementById("dialog-" + dlg);
 	if (dlg != null) {
 		let ele = dlg.parentElement;
-		if (!ele.classList.contains("block-dynamic")) {
-			ele.classList.remove("block-active");
-		} else {
-			ele.parentElement.removeChild(ele);
-		}
+		!ele.classList.contains("block-dynamic") ? ele.classList.remove("block-active") : ele.remove();
 	}
 	return dlg;
 }
 
 (function () {
+	let eles = document.getElementsByTagName("template");
+	while (eles.length > 0) {
+		eles[0].remove();
+	}
 	document.getElementById("nav-more").addEventListener("click", (e) => { openDialog("file"); });
 	document.getElementById("dialog-file-pool").addEventListener("change", function (e) {
 		if (this.files.length != 1)

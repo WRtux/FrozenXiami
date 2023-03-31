783c1a105a15e834251d16b763a921fdd3ec11e2
Wilderness Ranger <wildernessrgr@aliyun.com>
2023-03-31 22:21:58 +0800

    Pick: Update helper functions
    Update helper and prepare for rewriting of data pool implementation.

--- source/scripts/helper.js
+++ source/scripts/helper.js
@@ -4,31 +4,84 @@
  */
 "use strict";
 
+void function () {
+	let proto = {...EventTarget.prototype};
+	// Temporary polyfill. Signals are not natively supported on some targets.
+	EventTarget.prototype.addEventListener = function (type, handler, options = null) {
+		options = options ?? {};
+		let {signal, ...restOpts} = options;
+		if (signal != null && !(signal instanceof AbortSignal))
+			throw new TypeError();
+		proto.addEventListener.call(this, type, handler, restOpts);
+		if (signal != null)
+			signal.addEventListener('abort', (e) => this.removeEventListener(type, handler, restOpts));
+	};
+}();
+
 var H = {
-	buildHexadecimal(i, len) {
-		i = Number.parseInt(i);
-		return !Number.isNaN(i) ? "0x" + i.toString(16).padStart(len, '0').toUpperCase() : null;
+	formatHexadec(i, len = 2) {
+		i = Number(i);
+		if (Number.isNaN(i))
+			return 'NaN';
+		let sign = (Math.sign(i) !== -1 ? '' : '-');
+		i = Math.floor(Math.abs(i));
+		return sign + '0x' + i.toString(16).padStart(len, '0').toUpperCase();
 	},
+	/** @deprecated */
+	buildHexadecimal: (i, len) => H.formatHexadec(i, len),
 	filterFunction(func) {
 		return typeof(func) == "function" ? func : null;
 	},
 	filterGetFunction(func) {
 		return typeof(func) == "function" ? func : () => {};
 	},
-	readFileP(f) {
-		return new Promise(function (resolve, reject) {
-			let fr = new FileReader();
-			fr.readAsArrayBuffer(f);
-			fr.addEventListener("load", (e) => { resolve(e.target.result); });
-			fr.addEventListener("error", (e) => { reject(); });
-		});
-	},
-	restP() {
-		return new Promise((resolve, reject) => { window.setTimeout(resolve, 0); });
-	},
+	/** @deprecated */
 	sleepP(t) {
-		return new Promise((resolve, reject) => { window.setTimeout(resolve, t); });
+		return new Promise((resolve, reject) => window.setTimeout(resolve, t));
 	},
+	/** @deprecated */
+	restP() {
+		return H.sleepP(0);
+	},
+	fileSlice(f, start, end = null) {
+		if (!(f instanceof Blob))
+			throw new TypeError();
+		if (end != null && !(end >= 0 && end <= f.size))
+			throw new RangeError('File position out of range.');
+		return f.slice(start, end ?? undefined);
+	},
+	_fileRead(f, start, end, enc, signal) {
+		if (signal != null && !(signal instanceof AbortSignal))
+			throw new TypeError();
+		f = H.fileSlice(f, start, end);
+		let stub, notify, notifyError;
+		stub = new Promise((...args) => [notify, notifyError] = args);
+		let fr = new FileReader();
+		enc != null ?
+			fr.readAsText(f, enc) :
+			fr.readAsArrayBuffer(f);
+		if (signal != null) {
+			!signal.aborted ?
+				signal.addEventListener('abort', (e) => fr.abort()) :
+				fr.abort();
+		}
+		fr.addEventListener('load',  (e) => notify(fr.result), {signal});
+		fr.addEventListener('abort', (e) => notifyError(fr.error));
+		fr.addEventListener('error', (e) => notifyError(fr.error), {signal});
+		return stub;
+	},
+	fileReadArrayBuffer(f, start = null, end = null, signal = null) {
+		return H._fileRead(f, start, end, null, signal);
+	},
+	fileReadToDataView(f, start = null, end = null, signal = null) {
+		return H.fileReadArrayBuffer(f, start, end, signal)
+			.then((buf) => new DataView(buf));
+	},
+	fileReadString(f, start = null, end = null, enc = null, signal = null) {
+		return H._fileRead(f, start, end, enc ?? 'UTF-8', signal);
+	},
+	/** @deprecated */
+	readFileP: (f) => H.fileReadArrayBuffer(f),
 	callOnReadyP(func) {
 		return new Promise(function (resolve, reject) {
 			if (document.readyState != "loading") {
@@ -56,6 +109,7 @@ var H = {
 			ele.textContent = txt;
 		return ele;
 	},
+	/** @deprecated */
 	buildInfoRow(tt, infs) {
 		let dfrg = document.createDocumentFragment();
 		dfrg.appendChild(this.buildElement("dt", null, tt));
@@ -76,6 +130,56 @@ var H = {
 	removeChildren(cont) {
 		while (cont.hasChildNodes()) {
 			cont.removeChild(cont.firstChild);
+		}
+	},
+	eventBuildMessage(data) {
+		return new MessageEvent('message', {data});
+	},
+	eventResolveMap(etarget, map) {
+		if (!(etarget instanceof EventTarget))
+			throw new TypeError();
+		let stub, notify, notifyError;
+		stub = new Promise((...args) => [notify, notifyError] = args);
+		let aborter = new AbortController(), signal = aborter.signal;
+		for (let typ in map) {
+			if (map[typ] == null)
+				continue;
+			etarget.addEventListener(typ, (e) => {
+				try {
+					let complete = map[typ](e);
+					if (typeof complete === 'boolean' && complete) {
+						aborter.abort();
+						return notify();
+					}
+				} catch (err) {
+					aborter.abort();
+					return notifyError(err);
+				}
+			}, {signal});
+		}
+		return stub;
+	},
+	eventResolve(etarget, type, handler) {
+		return H.eventResolveMap(etarget, {[type]: handler});
+	},
+	workerBuildAbortable(url, signal) {
+		if (signal != null && !(signal instanceof AbortSignal))
+			throw new TypeError();
+		let worker = new Worker(url);
+		function abort() {
+			worker.dispatchEvent(new ProgressEvent('abort'));
+			worker.terminate();
+		}
+		if (signal != null) {
+			!signal.aborted ?
+				signal.addEventListener('abort', abort) :
+				abort();
+		}
+		return worker;
+	},
+	Messenger: class extends EventTarget {
+		_postMessage(msg) {
+			this.dispatchEvent(H.eventBuildMessage(msg));
 		}
 	}
 };
